ed3f18fc326f5e0b7174cf0c5467a1f954b0d76a
Max Kontak <Max.Kontak@DLR.de>
2019-05-13 16:31:02 +0200

    clustering_result: documentation in unit test clustering_result: documentation in unit test# Bitte geben Sie eine Commit-Beschreibung für Ihre Änderungen ein. Zeilen,

--- clustering_result_test.cpp
+++ clustering_result_test.cpp
@@ -13,12 +13,13 @@
 
 auto const tol{ std::sqrt(std::numeric_limits<double>::epsilon()) };
 
-TEST( Clustering, Initialization )
+TEST( ClusteringResult, Initialization )
 {
-  std::vector< std::vector< double > > distancesPerPointPerCluster = { {0.0, 1.0, 2.0},
-                                                                       {3.0, 4.0},
-                                                                       {5.0},
-                                                                       {6.0, 7.0, 8.0, 9.0, 10.0} };
+  // provide input for ClusteringResult constructor
+  std::vector< std::vector< double > > const distancesPerPointPerCluster = { {0.0, 1.0, 2.0},
+                                                                             {3.0, 4.0},
+                                                                             {5.0},
+                                                                             {6.0, 7.0, 8.0, 9.0, 10.0} };
 
   ClusteringResult< double > const clustering{ distancesPerPointPerCluster };
 
