ca360a6a59297bda2c9f236c6463714f367ad53f
Max Kontak <Max.Kontak@DLR.de>
2019-05-16 14:16:02 +0200

    kmeans: add test for second likelihood

--- kmeans_test.cpp
+++ kmeans_test.cpp
@@ -25,11 +25,11 @@ TEST( KMeans, lloyd_algorithm )
 #ifdef NDEBUG
   // larger numbers in release mode
   std::vector<vec>::size_type const constexpr maxK{ 10 };
-  std::vector<vec>::size_type const constexpr sampleSize{ 1000000 };
+  std::vector<vec>::size_type const constexpr sampleSizePerCluster{ 1000000 };
   std::vector<vec>::size_type const constexpr maxIter{ 10*maxK };
 #else
   std::vector<vec>::size_type const constexpr maxK{ 5 };
-  std::vector<vec>::size_type const constexpr sampleSize{ 1000 };
+  std::vector<vec>::size_type const constexpr sampleSizePerCluster{ 1000 };
   std::vector<vec>::size_type const constexpr maxIter{ 4*maxK };
 #endif
 
@@ -42,67 +42,126 @@ TEST( KMeans, lloyd_algorithm )
   // data vector
 
   std::vector< vec > data;
-  data.resize(3*sampleSize);
+  data.resize(3*sampleSizePerCluster);
 
   // construct 100 points around (0,0)
-  for (auto i=0U*sampleSize; i<1U*sampleSize; ++i)
+  for (auto i=0U*sampleSizePerCluster; i<1U*sampleSizePerCluster; ++i)
   {
     data[i][0] = 0 + gaussian(gen);
     data[i][1] = 0 + gaussian(gen);
   }
 
   // construct 100 points around (10,0)
-  for (auto i=1U*sampleSize; i<2U*sampleSize; ++i)
+  for (auto i=1U*sampleSizePerCluster; i<2U*sampleSizePerCluster; ++i)
   {
     data[i][0] = 10 + gaussian(gen);
     data[i][1] =  0 + gaussian(gen);
   }
 
   // construct 100 points around (0,10)
-  for (auto i=2U*sampleSize; i<3U*sampleSize; ++i)
+  for (auto i=2U*sampleSizePerCluster; i<3U*sampleSizePerCluster; ++i)
   {
     data[i][0] =  0 + gaussian(gen);
     data[i][1] = 10 + gaussian(gen);
   }
 
-  std::array< double, maxK+1 > likelihoods{};
-  std::array< double, maxK+1 > AICValues{};
-  std::array< double, maxK+1 > AICcValues{};
-  std::array< double, maxK+1 > BICValues{};
+  auto const sampleSize{ 3U*sampleSizePerCluster };
 
-  for (int k=1; k<=maxK; ++k)
+  std::vector<ClusteringResult<double>> clusterings{};
+
+  // do clusterings
+  for (int k=0; k<maxK; ++k)
   {
-    auto const clustering{ KMeans::lloyd_algorithm( data, k, maxIter ) };
-    auto const likelihood{ Likelihoods::residualSumOfSquares(clustering) };
-    likelihoods[k] = likelihood;
-
-    // for clustering, we have 2*k+N effective parameters:
-    // - the two coordinates of the k centroids (2*k parameters)
-    // - for each point, the information to which cluster it belongs (N parameters)
-    AICValues[k] = AkaikeInformationCriterion( likelihood, 2*k+sampleSize );
-    AICcValues[k] = AkaikeInformationCriterionCorrected( likelihood, 2*k+sampleSize, 3*sampleSize );
-    BICValues[k] = BayesianInformationCriterion( likelihood, 2*k+sampleSize, 3*sampleSize );
+    clusterings.emplace_back( KMeans::lloyd_algorithm( data, k+1, maxIter ) );
   }
-  std::cout << std::endl;
 
-  std::cout << "log(AIC): \n";
-  for (int k=1; k<=maxK; ++k)
   {
-    std::cout << "  k=" << k << ": " << std::log(AICValues[k]) << "\n";
-  }
-  std::cout << std::endl;
+    // clustering with residual sum of squares as likelihood (assumption: unit variance)
+    std::array< double, maxK > likelihoods{};
+    std::array< double, maxK > AICValues{};
+    std::array< double, maxK > AICcValues{};
+    std::array< double, maxK > BICValues{};
 
-  std::cout << "log(AICc): \n";
-  for (int k=1; k<=maxK; ++k)
-  {
-    std::cout << "  k=" << k << ": " << AICcValues[k] << "\n";
-  }
-  std::cout << std::endl;
+    for (size_t k=0; k<maxK; ++k)
+    {
+      auto const likelihood{ Likelihoods::unitVariance(clusterings[k]) };
+      likelihoods[k] = likelihood;
 
-  std::cout << "log(BIC): \n";
-  for (int k=1; k<=maxK; ++k)
-  {
-    std::cout << "  k=" << k << ": " << BICValues[k] << "\n";
+      // Established implementations use "2*#clusters" as effective number of parameters, motivated by the 2 entries per cluster centroid.
+      // Looking at the statistical model, this is not totally clear, because another parameter is to which cluster each sample belongs.
+      // Nevertheless, it works very well!
+      auto const numEffectiveParam{ 2*(k+1) };
+
+      AICValues[k]  = AkaikeInformationCriterion          ( likelihood, numEffectiveParam );
+      AICcValues[k] = AkaikeInformationCriterionCorrected ( likelihood, numEffectiveParam, sampleSize );
+      BICValues[k]  = BayesianInformationCriterion        ( likelihood, numEffectiveParam, sampleSize );
+    }
+
+    std::cout << "Clustering with likelihood: residual sum of squares\n\n";
+    std::cout << "AIC: \n";
+    for (int k=0; k<maxK; ++k)
+    {
+      std::cout << "  k=" << k+1 << ": " << (AICValues[k]) << "\n";
+    }
+    std::cout << std::endl;
+
+    std::cout << "AICc: \n";
+    for (int k=0; k<maxK; ++k)
+    {
+      std::cout << "  k=" << k+1 << ": " << (AICcValues[k]) << "\n";
+    }
+    std::cout << std::endl;
+
+    std::cout << "BIC: \n";
+    for (int k=0; k<maxK; ++k)
+    {
+      std::cout << "  k=" << k+1 << ": " << (BICValues[k]) << "\n";
+    }
+    std::cout << std::endl;
   }
-  std::cout << std::endl;
+
+  {
+    // clustering with log-likelihood function corresponding to the assumption of same variance per cluster
+    std::array< double, maxK > likelihoods{};
+    std::array< double, maxK > AICValues{};
+    std::array< double, maxK > AICcValues{};
+    std::array< double, maxK > BICValues{};
+
+    for (size_t k=0; k<maxK; ++k)
+    {
+      // this likelihood is based on the assumption that all clusters have the same variance
+      auto const likelihood{ Likelihoods::sameVariance(clusterings[k], 2U) };
+      likelihoods[k] = likelihood;
+
+      // the number of effective parameters consists of:
+      // 2*#Clusters          parameters for the centroids' components
+      // #Clusters*#samples   parameters for the binary variables, which encode if a data point belongs to a cluster
+      auto const numEffectiveParam{ 2*(k+1) + (k+1)*sampleSize };
+      AICValues[k]  = AkaikeInformationCriterion          ( likelihood, numEffectiveParam );
+      AICcValues[k] = AkaikeInformationCriterionCorrected ( likelihood, numEffectiveParam, sampleSize );
+      BICValues[k]  = BayesianInformationCriterion        ( likelihood, numEffectiveParam, sampleSize );
+    }
+
+    std::cout << "Clustering with likelihood: same variance\n\n";
+    std::cout << "AIC: \n";
+    for (int k=0; k<maxK; ++k)
+    {
+      std::cout << "  k=" << k+1 << ": " << (AICValues[k]) << "\n";
+    }
+    std::cout << std::endl;
+
+    std::cout << "AICc: \n";
+    for (int k=0; k<maxK; ++k)
+    {
+      std::cout << "  k=" << k+1 << ": " << (AICcValues[k]) << "\n";
+    }
+    std::cout << std::endl;
+
+    std::cout << "BIC: \n";
+    for (int k=0; k<maxK; ++k)
+    {
+      std::cout << "  k=" << k+1 << ": " << (BICValues[k]) << "\n";
+    }
+    std::cout << std::endl;
+  }
 }
