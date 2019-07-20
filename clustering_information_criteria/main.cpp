59e919e8403131f2ec44e1f0fba7ea3eeb2250f5
julesvidal <jules.vidal@lip6.fr>
2019-07-20 10:20:16 +0200

    added exact and approximate distances for all data-sets

--- clustering_information_criteria/main.cpp
+++ clustering_information_criteria/main.cpp
@@ -55,6 +55,247 @@ decltype(auto) sortVectorByOtherVector( std::vector<T1> const & toSort,
   return outputVector;
 }
 
+void gaussians()
+{
+  auto const dimension{ 1 };
+
+  std::vector< int > const numClusters{ 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  auto const I{ numClusters.size() };
+
+  std::vector< std::string > filenames = { "../../data/gaussians/distance_real_results/distances_100s_k2.txt",
+                                           "../../data/gaussians/distance_real_results/distances_100s_k3.txt",
+                                           "../../data/gaussians/distance_real_results/distances_100s_k4.txt",
+                                           "../../data/gaussians/distance_real_results/distances_100s_k5.txt",
+                                           "../../data/gaussians/distance_real_results/distances_100s_k6.txt",
+                                           "../../data/gaussians/distance_real_results/distances_100s_k7.txt",
+                                           "../../data/gaussians/distance_real_results/distances_100s_k8.txt",
+                                           "../../data/gaussians/distance_real_results/distances_100s_k9.txt",
+                                           "../../data/gaussians/distance_real_results/distances_100s_k10.txt" };
+
+  std::vector< double > likelihoods( I );
+  std::vector< double > AICValues  ( I );
+  std::vector< double > AICcValues ( I );
+  std::vector< double > BICValues  ( I );
+
+  for ( int i=0; i<I; ++i )
+  {
+    auto const clustering{ clusteringResultFromASCIIFile< double >( filenames[i] ) };
+
+    likelihoods[i] = Likelihoods::sameVariance(clustering, dimension);
+
+    decltype(I) const numEffectiveParam = dimension*numClusters[i];
+    auto const sampleSize{ clustering.numberOfPoints() };
+
+    AICValues[i]  = AkaikeInformationCriterion          ( likelihoods[i], numEffectiveParam );
+    AICcValues[i] = AkaikeInformationCriterionCorrected ( likelihoods[i], numEffectiveParam, sampleSize );
+    BICValues[i]  = BayesianInformationCriterion        ( likelihoods[i], numEffectiveParam, sampleSize );
+  }
+
+  std::cout << "Clustering with likelihood: unitVariance\n\n";
+  std::cout << "AIC (normalized wrt value at k=2): \n";
+  for (int i=0; i<I; ++i)
+  {
+    std::cout << "  k=" << numClusters[i] << ": " << AICValues[i]/std::abs(AICValues[0]) << "\n";
+  }
+  std::cout << std::endl;
+
+  auto const sortedNumClustersAIC{ sortVectorByOtherVector( numClusters, AICValues )};
+  std::cout << "k sorted by AIC value: ";
+  for ( auto v : sortedNumClustersAIC )
+    std::cout << v << ", ";
+
+  std::cout << std::endl << std::endl;
+
+  std::cout << "AICc (normalized wrt value at k=2): \n";
+  for (int i=0; i<I; ++i)
+  {
+    std::cout << "  k=" << numClusters[i] << ": " << AICcValues[i]/std::abs(AICcValues[0]) << "\n";
+  }
+  std::cout << std::endl;
+
+  auto const sortedNumClustersAICc{ sortVectorByOtherVector( numClusters, AICcValues )};
+  std::cout << "k sorted by AICc value: ";
+  for ( auto v : sortedNumClustersAICc )
+    std::cout << v << ", ";
+
+  std::cout << std::endl << std::endl;
+
+  std::cout << "BIC (normalized wrt value at k=2): \n";
+  for (int i=0; i<I; ++i)
+  {
+    std::cout << "  k=" << numClusters[i] << ": " << BICValues[i]/std::abs(BICValues[0]) << "\n";
+  }
+  std::cout << std::endl;
+
+  auto const sortedNumClustersBIC{ sortVectorByOtherVector( numClusters, BICValues )};
+  std::cout << "k sorted by BIC value: ";
+  for ( auto v : sortedNumClustersBIC )
+    std::cout << v << ", ";
+
+  std::cout << std::endl << std::endl;
+}
+
+
+void startingVortex()
+{
+  auto const dimension{ 1 };
+
+  std::vector< int > const numClusters{ 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  auto const I{ numClusters.size() };
+
+  std::vector< std::string > filenames = { "../../data/startingVortex/distance_real_results/distances_100s_k2.txt",
+                                           "../../data/startingVortex/distance_real_results/distances_100s_k3.txt",
+                                           "../../data/startingVortex/distance_real_results/distances_100s_k4.txt",
+                                           "../../data/startingVortex/distance_real_results/distances_100s_k5.txt",
+                                           "../../data/startingVortex/distance_real_results/distances_100s_k6.txt",
+                                           "../../data/startingVortex/distance_real_results/distances_100s_k7.txt",
+                                           "../../data/startingVortex/distance_real_results/distances_100s_k8.txt",
+                                           "../../data/startingVortex/distance_real_results/distances_100s_k9.txt",
+                                           "../../data/startingVortex/distance_real_results/distances_100s_k10.txt" };
+
+  std::vector< double > likelihoods( I );
+  std::vector< double > AICValues  ( I );
+  std::vector< double > AICcValues ( I );
+  std::vector< double > BICValues  ( I );
+
+  for ( int i=0; i<I; ++i )
+  {
+    auto const clustering{ clusteringResultFromASCIIFile< double >( filenames[i] ) };
+
+    likelihoods[i] = Likelihoods::sameVariance(clustering, dimension);
+
+    decltype(I) const numEffectiveParam = dimension*numClusters[i];
+    auto const sampleSize{ clustering.numberOfPoints() };
+
+    AICValues[i]  = AkaikeInformationCriterion          ( likelihoods[i], numEffectiveParam );
+    AICcValues[i] = AkaikeInformationCriterionCorrected ( likelihoods[i], numEffectiveParam, sampleSize );
+    BICValues[i]  = BayesianInformationCriterion        ( likelihoods[i], numEffectiveParam, sampleSize );
+  }
+
+  std::cout << "Clustering with likelihood: unitVariance\n\n";
+  std::cout << "AIC (normalized wrt value at k=2): \n";
+  for (int i=0; i<I; ++i)
+  {
+    std::cout << "  k=" << numClusters[i] << ": " << AICValues[i]/std::abs(AICValues[0]) << "\n";
+  }
+  std::cout << std::endl;
+
+  auto const sortedNumClustersAIC{ sortVectorByOtherVector( numClusters, AICValues )};
+  std::cout << "k sorted by AIC value: ";
+  for ( auto v : sortedNumClustersAIC )
+    std::cout << v << ", ";
+
+  std::cout << std::endl << std::endl;
+
+  std::cout << "AICc (normalized wrt value at k=2): \n";
+  for (int i=0; i<I; ++i)
+  {
+    std::cout << "  k=" << numClusters[i] << ": " << AICcValues[i]/std::abs(AICcValues[0]) << "\n";
+  }
+  std::cout << std::endl;
+
+  auto const sortedNumClustersAICc{ sortVectorByOtherVector( numClusters, AICcValues )};
+  std::cout << "k sorted by AICc value: ";
+  for ( auto v : sortedNumClustersAICc )
+    std::cout << v << ", ";
+
+  std::cout << std::endl << std::endl;
+
+  std::cout << "BIC (normalized wrt value at k=2): \n";
+  for (int i=0; i<I; ++i)
+  {
+    std::cout << "  k=" << numClusters[i] << ": " << BICValues[i]/std::abs(BICValues[0]) << "\n";
+  }
+  std::cout << std::endl;
+
+  auto const sortedNumClustersBIC{ sortVectorByOtherVector( numClusters, BICValues )};
+  std::cout << "k sorted by BIC value: ";
+  for ( auto v : sortedNumClustersBIC )
+    std::cout << v << ", ";
+
+  std::cout << std::endl << std::endl;
+}
+void vortexStreet()
+{
+  auto const dimension{ 1 };
+
+  std::vector< int > const numClusters{ 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  auto const I{ numClusters.size() };
+
+  std::vector< std::string > filenames = { "../../data/vortexStreet/distance_real_results/distances_100s_k2.txt",
+                                           "../../data/vortexStreet/distance_real_results/distances_100s_k3.txt",
+                                           "../../data/vortexStreet/distance_real_results/distances_100s_k4.txt",
+                                           "../../data/vortexStreet/distance_real_results/distances_100s_k5.txt",
+                                           "../../data/vortexStreet/distance_real_results/distances_100s_k6.txt",
+                                           "../../data/vortexStreet/distance_real_results/distances_100s_k7.txt",
+                                           "../../data/vortexStreet/distance_real_results/distances_100s_k8.txt",
+                                           "../../data/vortexStreet/distance_real_results/distances_100s_k9.txt",
+                                           "../../data/vortexStreet/distance_real_results/distances_100s_k10.txt" };
+
+  std::vector< double > likelihoods( I );
+  std::vector< double > AICValues  ( I );
+  std::vector< double > AICcValues ( I );
+  std::vector< double > BICValues  ( I );
+
+  for ( int i=0; i<I; ++i )
+  {
+    auto const clustering{ clusteringResultFromASCIIFile< double >( filenames[i] ) };
+
+    likelihoods[i] = Likelihoods::sameVariance(clustering, dimension);
+
+    decltype(I) const numEffectiveParam = dimension*numClusters[i];
+    auto const sampleSize{ clustering.numberOfPoints() };
+
+    AICValues[i]  = AkaikeInformationCriterion          ( likelihoods[i], numEffectiveParam );
+    AICcValues[i] = AkaikeInformationCriterionCorrected ( likelihoods[i], numEffectiveParam, sampleSize );
+    BICValues[i]  = BayesianInformationCriterion        ( likelihoods[i], numEffectiveParam, sampleSize );
+  }
+
+  std::cout << "Clustering with likelihood: unitVariance\n\n";
+  std::cout << "AIC (normalized wrt value at k=2): \n";
+  for (int i=0; i<I; ++i)
+  {
+    std::cout << "  k=" << numClusters[i] << ": " << AICValues[i]/std::abs(AICValues[0]) << "\n";
+  }
+  std::cout << std::endl;
+
+  auto const sortedNumClustersAIC{ sortVectorByOtherVector( numClusters, AICValues )};
+  std::cout << "k sorted by AIC value: ";
+  for ( auto v : sortedNumClustersAIC )
+    std::cout << v << ", ";
+
+  std::cout << std::endl << std::endl;
+
+  std::cout << "AICc (normalized wrt value at k=2): \n";
+  for (int i=0; i<I; ++i)
+  {
+    std::cout << "  k=" << numClusters[i] << ": " << AICcValues[i]/std::abs(AICcValues[0]) << "\n";
+  }
+  std::cout << std::endl;
+
+  auto const sortedNumClustersAICc{ sortVectorByOtherVector( numClusters, AICcValues )};
+  std::cout << "k sorted by AICc value: ";
+  for ( auto v : sortedNumClustersAICc )
+    std::cout << v << ", ";
+
+  std::cout << std::endl << std::endl;
+
+  std::cout << "BIC (normalized wrt value at k=2): \n";
+  for (int i=0; i<I; ++i)
+  {
+    std::cout << "  k=" << numClusters[i] << ": " << BICValues[i]/std::abs(BICValues[0]) << "\n";
+  }
+  std::cout << std::endl;
+
+  auto const sortedNumClustersBIC{ sortVectorByOtherVector( numClusters, BICValues )};
+  std::cout << "k sorted by BIC value: ";
+  for ( auto v : sortedNumClustersBIC )
+    std::cout << v << ", ";
+
+  std::cout << std::endl << std::endl;
+}
+
+
 void seaSurfaceHeight()
 {
   auto const dimension{ 1 };
@@ -62,15 +303,15 @@ void seaSurfaceHeight()
   std::vector< int > const numClusters{ 2, 3, 4, 5, 6, 7, 8, 9, 10 };
   auto const I{ numClusters.size() };
 
-  std::vector< std::string > filenames = { "/localdata1/kont_ma/VESTEC/vestec-wp3/data/seaSurfaceHeight/ssh_100s_k2.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/seaSurfaceHeight/ssh_100s_k3.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/seaSurfaceHeight/ssh_100s_k4.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/seaSurfaceHeight/ssh_100s_k5.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/seaSurfaceHeight/ssh_100s_k6.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/seaSurfaceHeight/ssh_100s_k7.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/seaSurfaceHeight/ssh_100s_k8.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/seaSurfaceHeight/ssh_100s_k9.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/seaSurfaceHeight/ssh_100s_k10.txt" };
+  std::vector< std::string > filenames = { "../../data/seaSurfaceHeight/distance_real_results/distances_100s_k2.txt",
+                                           "../../data/seaSurfaceHeight/distance_real_results/distances_100s_k3.txt",
+                                           "../../data/seaSurfaceHeight/distance_real_results/distances_100s_k4.txt",
+                                           "../../data/seaSurfaceHeight/distance_real_results/distances_100s_k5.txt",
+                                           "../../data/seaSurfaceHeight/distance_real_results/distances_100s_k6.txt",
+                                           "../../data/seaSurfaceHeight/distance_real_results/distances_100s_k7.txt",
+                                           "../../data/seaSurfaceHeight/distance_real_results/distances_100s_k8.txt",
+                                           "../../data/seaSurfaceHeight/distance_real_results/distances_100s_k9.txt",
+                                           "../../data/seaSurfaceHeight/distance_real_results/distances_100s_k10.txt" };
 
   std::vector< double > likelihoods( I );
   std::vector< double > AICValues  ( I );
@@ -142,15 +383,15 @@ void isabel()
   std::vector< int > const numClusters{ 2, 3, 4, 5, 6, 7, 8, 9, 10 };
   auto const I{ numClusters.size() };
 
-  std::vector< std::string > filenames = { "/localdata1/kont_ma/VESTEC/vestec-wp3/data/isabel/isabel_100s_k2.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/isabel/isabel_100s_k3.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/isabel/isabel_100s_k4.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/isabel/isabel_100s_k5.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/isabel/isabel_100s_k6.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/isabel/isabel_100s_k7.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/isabel/isabel_100s_k8.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/isabel/isabel_100s_k9.txt",
-                                           "/localdata1/kont_ma/VESTEC/vestec-wp3/data/isabel/isabel_100s_k10.txt" };
+  std::vector< std::string > filenames = { "../../data/isabel/distance_real_results/distances_100s_k2.txt",
+                                           "../../data/isabel/distance_real_results/distances_100s_k3.txt",
+                                           "../../data/isabel/distance_real_results/distances_100s_k4.txt",
+                                           "../../data/isabel/distance_real_results/distances_100s_k5.txt",
+                                           "../../data/isabel/distance_real_results/distances_100s_k6.txt",
+                                           "../../data/isabel/distance_real_results/distances_100s_k7.txt",
+                                           "../../data/isabel/distance_real_results/distances_100s_k8.txt",
+                                           "../../data/isabel/distance_real_results/distances_100s_k9.txt",
+                                           "../../data/isabel/distance_real_results/distances_100s_k10.txt" };
 
   std::vector< double > AICValues  ( I );
   std::vector< double > AICcValues ( I );
@@ -216,14 +457,30 @@ void isabel()
 
 int main()
 {
-  std::cout << "sea surface height data set:\n";
-  std::cout << "============================";
-  std::cout << "expected result: 4 clusters\n";
-  seaSurfaceHeight();
-  std::cout << std::endl;
+
+  std::cout << "gaussians data set:\n";
+  std::cout << "================";
+  std::cout << "expected result: 3 clusters\n";
+  gaussians();
+
+  std::cout << "vortexStreet data set:\n";
+  std::cout << "================";
+  std::cout << "expected result: 5 clusters\n";
+  vortexStreet();
+
+  std::cout << "startingVortex data set:\n";
+  std::cout << "================";
+  std::cout << "expected result: 2 clusters\n";
+  startingVortex();
 
   std::cout << "isabel data set:\n";
   std::cout << "================";
   std::cout << "expected result: 3 clusters\n";
   isabel();
+
+  std::cout << "sea surface height data set:\n";
+  std::cout << "============================";
+  std::cout << "expected result: 4 clusters\n";
+  seaSurfaceHeight();
+  std::cout << std::endl;
 }
