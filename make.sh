b5b5c44484da1828045710c3ce53eb6edf08e387
Fellegara, Riccardo <riccardo.fellegara@dlr.de>
2020-02-17 15:11:36 +0100

    added bash script for compiling TTK on DLR workstations (based on SLED12)

---
+++ make.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# prerequisites:
+#     git clone git@github.com:julien-tierny/vestec-wp3.git 
+#     git checkout space_weather_prototype_DLR_config
+#     git submodule update --init
+
+# then, execute this script in the folder containing the repository and submodules..
+
+NUMPY_DIR=$1
+if [ "$NUMPY_DIR" == "" ]; then
+   echo "[error] the directory containing numpy C headers has to be given as argument! exiting..."
+   exit 1;
+fi
+
+# Get the current directory - this is the default location for the build and install directory.
+CURRENT_DIR="$(pwd)"
+
+# number of build jobs (passed to make -j)
+NUM_BUILD_JOBS="$(nproc)"
+
+INSTALL_DIR="$CURRENT_DIR/install"
+BUILD_DIR="$CURRENT_DIR/build"
+
+### create build and install directories
+mkdir {build,install,build/qt5,install/qt5,install/paraview-ttk,build/paraview-ttk}
+mkdir {install/zfp,build/zfp,install/ttk,build/ttk,build/ipicmini}
+
+### 1 COMPILE QT (alternatively download the sources) 
+## (INFO: to understand the available flags that you can set type ./configure --help)
+cd $CURRENT_DIR/qt5
+git checkout 5.14
+#perl init-repository -f
+#### we initialize only the modules needed by paraview
+perl init-repository -f --module-subset=essential,qtx11extras
+#perl init-repository -f --module-subset=default,qtx11extras,-qtwebengine
+cd $BUILD_DIR/qt5
+../../qt5/configure -prefix "$INSTALL_DIR/qt5" -opensource -confirm-license -nomake tests -nomake examples -silent -release
+gmake "-j$NUM_BUILD_JOBS"
+gmake install
+
+### 1 COMPILE PARAVIEW
+cd $BUILD_DIR/paraview-ttk/
+#WARNING: OSPRAY is disabled at this moment
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/paraview-ttk -DQt5_DIR:PATH=$INSTALL_DIR/qt5/lib/cmake/Qt5 -DPARAVIEW_USE_RAYTRACING=OFF -DCMAKE_BUILD_TYPE=Release $CURRENT_DIR/paraview-ttk/
+cmake --build . --target install --parallel "$NUM_BUILD_JOBS"
+
+### 2 COMPILE ZFP
+# cd zfp
+cd $BUILD_DIR/zfp
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/zfp \
+	$CURRENT_DIR/zfp
+cmake --build . --target install --parallel "$NUM_BUILD_JOBS"
+
+
+### 3 COMPILE TTK
+cd $BUILD_DIR/ttk
+cmake DCMAKE_C_FLAGS="-luuid" \
+	-DCMAKE_CXX_FLAGS="-luuid" \
+	-DTTK_BUILD_STANDALONE_APPS=OFF \
+	-DTTK_ENABLE_ZFP=ON \
+	-DParaView_DIR:PATH=$BUILD_DIR/paraview-ttk \
+	-DZFP_DIR:PATH=$BUILD_DIR/zfp \
+	-DPYTHON_NUMPY_INCLUDE_DIR=$NUMPY_DIR \
+	-DTTK_ENABLE_GRAPHVIZ=OFF \
+	-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/ttk \
+	-DCMAKE_BUILD_TYPE=Release \
+	$CURRENT_DIR/ttk/
+cmake --build . --target install --parallel "$NUM_BUILD_JOBS"
+export PV_PLUGIN_PATH=$INSTALL_DIR/ttk/lib64/ttk/paraview-plugin
+
+### 4 COMPILE IPICMINI #####
+#mkdir install/ipicmini
+cd $BUILD_DIR/ipicmini
+cmake -DCMAKE_C_FLAGS="-luuid" \
+	-DCMAKE_CXX_FLAGS="-luuid" \
+	-DParaView_DIR:PATH=$BUILD_DIR/paraview-ttk \
+	-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/ipicmini \
+	-DUSE_CATALYST=ON \
+	-DCMAKE_BUILD_TYPE=Release \
+	$CURRENT_DIR/ipicmini/src
+cmake --build . --parallel "$NUM_BUILD_JOBS"
+
+# finally switch to repository
+cd $CURRENT_DIR
